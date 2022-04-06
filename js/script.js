6a30dc715c68ec4471a08083bdd3e8e90d0d0a43
Alexandr <zima2282@gmail.com>
2022-04-06 17:58:07 +0300

    3 level

--- js/script.js
+++ js/script.js
@@ -1,19 +1,62 @@
-let numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+    }
+};
+start();
 
 let personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-let a = prompt('Какой фильм', '');
-let b = prompt('Оценка', '');
-let c = prompt('Какой фильм', '');
-let d = prompt('Оценка', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt('Какой фильм', '');
+        let b = prompt('Оценка', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("Done");
+        } else {
+            i--;
+            console.log('Error');
+        }
+    }
+};
+// rememberMyFilms();
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Мало');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Norm');
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Cool");
+    } else {
+        console.log('Error');
+    };
+};
+// detectPersonalLevel();
+
+function showMyBD() {
+    if(personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    };
+};
+// showMyBD();
+
+function writeYourGenres() {
+    for(let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}?`, '');
+    };
+};
+writeYourGenres();
 
 console.log(personalMovieDB);
