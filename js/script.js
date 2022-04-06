08559428ffcf55505d6daf62adcddea1f50e0786
Alexandr <zima2282@gmail.com>
2022-04-06 11:12:53 +0300

    6.04

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('1111');
+let numberOfFilms = +prompt("Сколько фильмов вы просмотрели?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+let a = prompt('Какой фильм', '');
+let b = prompt('Оценка', '');
+let c = prompt('Какой фильм', '');
+let d = prompt('Оценка', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
